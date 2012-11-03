26c36be4bddb2ca1356ce9a104aa25953d2dd55a
Sol Eun <soleun@Sols-MacBook-Pro.local>
2012-11-02 23:35:57 -0700

    adding NewsItemTopTextCell

---
+++ HackerNewsReader/SENewsItemTopTextCell.h
@@ -0,0 +1,13 @@
+//
+//  SENewsItemTopTextCell.h
+//  HackerNewsReader
+//
+//  Created by Sol Eun on 11/2/12.
+//  Copyright (c) 2012 Sol Eun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SENewsItemTopTextCell : UITableViewCell
+
+@end
---
+++ HackerNewsReader/SENewsItemTopTextCell.m
@@ -0,0 +1,29 @@
+//
+//  SENewsItemTopTextCell.m
+//  HackerNewsReader
+//
+//  Created by Sol Eun on 11/2/12.
+//  Copyright (c) 2012 Sol Eun. All rights reserved.
+//
+
+#import "SENewsItemTopTextCell.h"
+
+@implementation SENewsItemTopTextCell
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
